bd0efced45ff4f892f9b9788b7759621962e235d
2003K <rachatask139@gmail.com>
2022-12-12 07:40:00 +0700

    00

--- backend/app.js
+++ backend/app.js
@@ -10,11 +10,11 @@ app.use("/books",router)
 
 mongoose
   .connect(
-    "mongodb+srv://user2:dsNuyRBYH8Yw0g6a@cluster0.zjprqqd.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://kan:Hm3yju34TfEUSd4p@cluster0.zjprqqd.mongodb.net/?retryWrites=true&w=majority"
   )
   .then(() => console.log("Connected To Database"))
   .then(() => {
-    app.listen(5000);
+    app.listen(5001);
     })
   .catch((err) => console.log(err));
 
